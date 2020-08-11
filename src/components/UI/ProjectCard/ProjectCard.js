545f406cd08747710ae6381de6a2631cafe9474b
madhav-karambelkar <mady18.1998@gmail.com>
2020-08-11 18:08:53 +0530

    Add Project Name

--- src/components/UI/ProjectCard/ProjectCard.js
+++ src/components/UI/ProjectCard/ProjectCard.js
@@ -1,14 +1,34 @@
-import React from "react"
-
+import React,{useState}from "react"
+import firebase from "firebase/app"
+import "firebase/database"
+import "firebase/auth"
 
 import "./ProjectCard.scss"
 
+
+
 export default function ProjectCard(props) {
+    
+    const database = firebase.database();
+    const refURL = 'users/' + firebase.auth().currentUser.uid +'/'
+    let [text , setText] = useState({});
+    const writeToDB = (evt) =>
+    {
+        evt.preventDefault();
+        //alert(`Submitting Name ${text}`)
+        //console.log("TEXT BEFORE :-",text);
+        database.ref(refURL).push(text);
+        //---- Reload Window --------
+        window.location.reload(false);
+    }
     if (props.type === "add") {
         return (
             <div className="project-card">
                 <div className="project-card-title">
-                    {props.title}
+                    <form onSubmit={writeToDB}>
+                        <input type="text" placeholder="Add Project Name" onChange={event =>  setText(event.target.value)}/>
+                        <button  type="submit">Send</button>
+                    </form>
                 </div>
             </div>)
     }
@@ -17,6 +37,9 @@ export default function ProjectCard(props) {
             <div className="project-card">
                 <div className="project-card-title">
                     {props.title}
+                    <span>
+                        <i class="fa fa-trash" aria-hidden="true"  ></i>
+                    </span>
                 </div>
             </div>
         )
--- src/components/pages/Dashboard/DashboardPage.js
+++ src/components/pages/Dashboard/DashboardPage.js
@@ -9,30 +9,35 @@ import "./DashboardPage.scss"
 
 export default function DashboardPage(props) {
     const database = firebase.database();
-    const refURL = 'users/' + firebase.auth().currentUser.uid
+    const refURL = 'users/' + firebase.auth().currentUser.uid +'/'
 
     let [projectCards, setProjectCards] = useState({});
-
+    let pro_key = [];
     function writeToDB(info) {
         database.ref(refURL).push(info);
     }
 
     function readFromDB() {
         // database.ref(refURL).on('value', (snapshot) => setProjectCards(snapshot.val()));
-        database.ref(refURL).once('value')
+        database.ref(`${refURL}`).once('value')
             .then((snapshot) => {
-                setProjectCards(snapshot.val())
+                console.log('Snapshot ', snapshot);
+                setProjectCards(snapshot.val());
             });
     }
 
     useEffect(() => readFromDB(), [])
 
     return (
-        <div>
+        <div >
             <MenuBar onLogout={props.onLogout} />
             <div className="dashboard-card-container">
                 <ProjectCard type="add" />
-                {Object.values(projectCards).map((title) => <ProjectCard title={title} />)}
+                { console.log("interpolated Project CArd \n" , projectCards)}
+                {
+                  Object.values(projectCards)
+                  .map((pro_id) => <ProjectCard title={pro_id} key={pro_id} />)
+                }
             </div>
         </div>
     )
