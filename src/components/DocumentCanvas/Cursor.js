32ac649200d465f3475df307b31e7946dcacbfbf
Dev Seth <dev.seth@duke.edu>
2020-10-01 22:59:58 +0530

    made cursor movement more natural, fixed positioning offset bug

--- src/components/DocumentCanvas/Cursor.js
+++ src/components/DocumentCanvas/Cursor.js
@@ -1,24 +1,30 @@
 import React, { useEffect } from "react";
 import gsap from "gsap"
 
+const MENUBAR_HEIGHT = 60;
 
 export default function Cursor(props) {
     useEffect(() => {
-        gsap.to("#cursor".concat(props.id), { x: props.x, y: props.y, duration: 0.8 }).play();
+        gsap.to("#cursor".concat(props.id), { left: props.x, top: props.y - MENUBAR_HEIGHT, duration: 0.2 }).play();
     }, [props.x, props.y, props.id])
 
     return (
         <div id={"cursor".concat(props.id)} style={{
             position: "absolute",
-            width: 40,
-            height: 40,
+            height: 45,
             display: "flex",
             flexFlow: "column nowrap",
             justifyContent: "space-between",
+            alignItems: "flex-start",
             overflow: "visible",
         }}>
-            <img alt="cursor" src={require("../../assets/cursor.svg")} style={{ userSelect: "none", maxWidth: "25px", maxHeight: "25px" }} />
-            <div style={{ backgroundColor: "darkgreen", color: "white", height: 16, fontSize: 13, fontFamily: "sans-serif" }}>
+            <img alt="cursor"
+                src={require("../../assets/cursor.svg")}
+                style={{ userSelect: "none", maxWidth: "25px", maxHeight: "25px" }} />
+            <div style={{
+                marginLeft: 10, paddingLeft: 5, paddingRight: 5, paddingBottom: 2, paddingTop: 2,
+                backgroundColor: "darkorange", color: "white", fontSize: 13, fontFamily: "sans-serif"
+            }}>
                 {props.name}
             </div>
         </div>
