728e9ed2e524f5768a3c4566b5abd92d0cb61136
Dev Seth <dev.seth@duke.edu>
2020-10-16 18:07:34 +0530

    added image auto resizing logic

--- src/constants/searchTemplate.js
+++ src/constants/searchTemplate.js
@@ -25,73 +25,76 @@ import MiniSearch from 'minisearch';
  * //Need to find name then 
  * indexes = [content.author.name] //indexes should be like this
  */
-export const searchElementinDocuments = (text , elementToBeSearchIn , indexes) =>{
-    const makeArrayofProject = [] ;
+export const searchElementinDocuments = (text, elementToBeSearchIn, indexes) => {
+    const makeArrayofProject = [];
     let miniSearch = new MiniSearch({
-        fields : indexes,
+        fields: indexes,
         extractField: (document, fieldName) => {
             // Access nested fields
             return fieldName.split('.').reduce((doc, key) => doc && doc[key], document)
         }
     });
-    
-    Object.entries(elementToBeSearchIn).map(([key,val])=>{
-        if(key!="root")
-        switch(val.type){
-            case "text" :
-                makeArrayofProject.push({id:key,content:val.content});
-                break;
-            case "todo" :
-                makeArrayofProject.push({id:key,title:val.content.title})
-                Object.entries(val.content.items).map(([keys,values])=>{
-                    if(keys!="root")
-                    makeArrayofProject.push({id:key,content:values , subID : keys})
-                });
-                break;
-            case "link" :
-                makeArrayofProject.push({id:key,content:val.content});
-                break;
-            case "pdf":
-                Object.entries(val.content).map(([[subkey , subvalues],values])=>{
-                     console.log("SUBKEY SUBVALUES ",subkey,subvalues,values)
-                    makeArrayofProject.push({id:key,fileName:values.metadata?.name.split(">")[0]})
-                });
-                break;
-            case "audio":
-                Object.entries(val.content).map(([[subkey , subvalues],values])=>{
-                    makeArrayofProject.push({id:key,fileName:values.metadata?.name.split(">")[0]})
-                });
-                break;
-            case "VideoFile":
-                Object.entries(val.content).map(([[subkey , subvalues],values])=>{
-                    makeArrayofProject.push({id:key,fileName:values.metadata?.name.split(">")[0]})
-                });
-                break;
-            case 'file':
-                Object.entries(val.content).map(([[subkey , subvalues],values])=>{
-                    makeArrayofProject.push({id:key,fileName:values.metadata?.name.split(">")[0]})
-                });
-                break;
-            case 'VideoLink':
-                Object.entries(val.content).map(([[subkey , subvalues],values])=>{
-                    makeArrayofProject.push({id:key,fileName:values.metadata?.name.split(">")[0]})
-                });
-                break;
-            case "blank":
-                makeArrayofProject.push({id:key,content:val.content});
-                break;
-            default :
-                //This is For Search in Cards Other than documents/projectID/nodes/
-                makeArrayofProject.push({id:key , content:val})
-                break;    
-        }
-        
+
+    // card manager -> minisearch import
+    // create minisearch index
+
+    Object.entries(elementToBeSearchIn).map(([key, val]) => {
+        if (key != "root")
+            switch (val.type) {
+                case "text":
+                    makeArrayofProject.push({ id: key, content: val.content });
+                    break;
+                case "todo":
+                    makeArrayofProject.push({ id: key, title: val.content.title })
+                    Object.entries(val.content.items).map(([keys, values]) => {
+                        if (keys != "root")
+                            makeArrayofProject.push({ id: key, content: values, subID: keys })
+                    });
+                    break;
+                case "link":
+                    makeArrayofProject.push({ id: key, content: val.content });
+                    break;
+                case "pdf":
+                    Object.entries(val.content).map(([[subkey, subvalues], values]) => {
+                        console.log("SUBKEY SUBVALUES ", subkey, subvalues, values)
+                        makeArrayofProject.push({ id: key, fileName: values.metadata?.name.split(">")[0] })
+                    });
+                    break;
+                case "audio":
+                    Object.entries(val.content).map(([[subkey, subvalues], values]) => {
+                        makeArrayofProject.push({ id: key, fileName: values.metadata?.name.split(">")[0] })
+                    });
+                    break;
+                case "VideoFile":
+                    Object.entries(val.content).map(([[subkey, subvalues], values]) => {
+                        makeArrayofProject.push({ id: key, fileName: values.metadata?.name.split(">")[0] })
+                    });
+                    break;
+                case 'file':
+                    Object.entries(val.content).map(([[subkey, subvalues], values]) => {
+                        makeArrayofProject.push({ id: key, fileName: values.metadata?.name.split(">")[0] })
+                    });
+                    break;
+                case 'VideoLink':
+                    Object.entries(val.content).map(([[subkey, subvalues], values]) => {
+                        makeArrayofProject.push({ id: key, fileName: values.metadata?.name.split(">")[0] })
+                    });
+                    break;
+                case "blank":
+                    makeArrayofProject.push({ id: key, content: val.content });
+                    break;
+                default:
+                    //This is For Search in Cards Other than documents/projectID/nodes/
+                    makeArrayofProject.push({ id: key, content: val })
+                    break;
+            }
+
     })
-    if(makeArrayofProject.length > 0 )
-     miniSearch.addAll(makeArrayofProject)
+    if (makeArrayofProject.length > 0)
+        miniSearch.addAll(makeArrayofProject)
 
     const results = miniSearch.search(text, { fuzzy: 0.2 });
-    
-    console.log("THE SEARCH ELEMENTS ",results , text,makeArrayofProject);
+
+    console.log("THE SEARCH ELEMENTS ", results, text, makeArrayofProject);
     return results;
 }
