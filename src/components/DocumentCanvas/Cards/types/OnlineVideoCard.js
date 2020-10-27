95c0ae443146196305c0911402b6959df1b26371
Dev Seth <dev.seth@duke.edu>
2020-10-27 14:49:45 +0530

    online video fix

--- src/components/DocumentCanvas/Cards/types/OnlineVideoCard.js
+++ src/components/DocumentCanvas/Cards/types/OnlineVideoCard.js
@@ -9,45 +9,39 @@ import ReactPlayer from 'react-player/lazy';
 const OnlineVideoCard = (props) => {
     //let multiplier,maxDimension;
     //const [maxDimension,setMaxDimension]=useState();
-    const [multiplier,setMultiplier]=useState();
-    const [dimension,setDimension] = useState();
-    useEffect(()=>{
+    const [multiplier, setMultiplier] = useState();
+    const [dimension, setDimension] = useState();
+    useEffect(() => {
         const changeSize = (height, width) => {
             props.typeAPI.resize(props.id, { width: width, height: height })
         }
-        if( props.content.metadata?.error === undefined)
-        {
-            let height= typeof props.content.metadata?.height ==='number' ?props.content.metadata?.height:350;
-            let width=typeof props.content.metadata?.width ==='number' ?props.content.metadata?.width:350;
-            let  temp = Math.max(height,width)
+        if (props.content.metadata?.error === undefined) {
+            let height = typeof props.content.metadata?.height === 'number' ? props.content.metadata?.height : 350;
+            let width = typeof props.content.metadata?.width === 'number' ? props.content.metadata?.width : 350;
+            let temp = Math.max(height, width)
             //setMaxDimension(temp);
-            setDimension({width:width , height:height})
+            setDimension({ width: width, height: height })
             setMultiplier(temp >= 350 ? 350 / temp : 1);
-            temp = temp >= 350 ? 350 / temp : 1 ;
-            console.log("CHANGES ",temp,Math.floor(height * temp) + 35);
-            console.log("CHANGES ",Math.floor(width  * temp));
-            changeSize(Math.floor(height * temp) + 50 , Math.floor(width  * temp))
+            temp = temp >= 350 ? 350 / temp : 1;
+            console.log("CHANGES ", temp, Math.floor(height * temp) + 35);
+            console.log("CHANGES ", Math.floor(width * temp));
+            changeSize(Math.floor(height * temp) + 50, Math.floor(width * temp))
         }
-    
-    },[props.content.metadata ,props.content.metadata.height,props.content.metadata.width,props.id,props.typeAPI , props.content.text])
-    //console.log("PROPS ",props.content.metadata , maxDimension,multiplier,parseInt(props.content.metadata?.width),Math.floor(parseInt(props.content.metadata?.height) * multiplier) + 5)
+
+    }, [props.content.metadata, props.id, props.typeAPI, props.content.text])
     return (
-        <>
-            {
-                dimension  && props.content?.text === undefined ?
-                <div>
-                    <ReactPlayer
-                        url={props.content.url}
-                        width={`${Math.floor(parseInt(dimension.width) * multiplier)}px`}
-                        height={`${Math.floor(parseInt(dimension.height )* multiplier)}px`} 
-                        controls={true}
-                        light={true}
-                    />
-                    <a style={{color:'red'}} href={props.content.metadata.url}>{props.content.metadata.title}</a>
-                </div>
-                : null
-            }
-        </>
+        dimension && props.content?.text === undefined ?
+            <div>
+                <ReactPlayer
+                    url={props.content.url}
+                    width={`${Math.floor(parseInt(dimension.width) * multiplier)}px`}
+                    height={`${Math.floor(parseInt(dimension.height) * multiplier)}px`}
+                    controls={true}
+                    light={true}
+                />
+                <a style={{ color: 'red' }} href={props.content.metadata.url}>{props.content.metadata.title}</a>
+            </div>
+            : null
     )
 }
 export default React.memo(OnlineVideoCard);
