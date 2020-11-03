9d1f673192887f516ad7b3e1a5f9f7b8ea62e340
Chinmay Agrawal <chinmayagrawal90@gmail.com>
2020-11-03 18:08:05 +0530

    added sass in usermenu

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import { usePopper } from "react-popper";
 import styled from "styled-components";
+import '../../styles/UserMenu.scss'
 
 const UserMenu = (props) => {
 
@@ -29,25 +30,24 @@ const UserMenu = (props) => {
             ]
         }
     );
-
     return (
-        <>
-        
-        <Page>
+
+
+        <div>
             <img alt="" onClick={() => setShowPopper(!showPopper)} className={props.imageClass} src={props.photoURL} ref={buttonRef} />
 
             {showPopper
-                ? <PopperContainer
+                ? (<div
                     ref={popperRef}
+                    className="tooltips"
                     style={styles.popper}
                     {...attributes.popper}
                 >
-                    <div ref={setArrowRef} style={styles.arrow} id="arrow" />
-                    <img src={props.photoURL} className="profimage" style={ProfImage}></img>
+                    <div ref={setArrowRef} style={styles.arrow} id="arrow" className="arrow" />
+                    <img src={props.photoURL} className="profimage"></img>
                     <h4>{props.username}</h4>
                     <p style={{ color: "orange" }}>groupthink <b>pro</b></p>
                     <hr />
-
                     <a href="#" target="blank" style={{ color: "black" }}>Profile</a>
                     <br />
                     <a href="/dashboard" style={{ color: "black" }}>Dashboard</a>
@@ -59,53 +59,9 @@ const UserMenu = (props) => {
                     <a href="" onClick={props.signOut} style={{ color: "orange" }}>Logout</a>
 
 
-                </PopperContainer> : null}
-        </Page>
-    </>);
+                </div>) : null}
+        </div>
+    );
 };
 
-const Page = styled.div`
-   width: 20%
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-`;
-
-const PopperContainer = styled.div`
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-  border: 1px solid black;
-  border-radius: 5px;
-  background-color: white;
-  padding: 10px;
-  text-align: center;
-
-  #arrow {
-    position: absolute;
-    width: 10px;
-    height: 10px;
-    &:after {
-      content: " ";
-      background-color: white;
-      box-shadow: -1px -1px 1px rgba(0, 0, 0, 0.1);
-      position: absolute;
-      top: -15px; // padding + popper height
-      left: 0;
-      transform: rotate(45deg);
-      width: 10px;
-      height: 10px;
-    }
-  }
-
-  &[data-popper-placement^='top'] > #arrow {
-    bottom: -20px;
-    :after {
-      box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.1);
-    }
-  }
-`;
-const ProfImage = {
-    borderRadius: '50%',
-    height: '100px',
-    width: '100px'
-}
 export default UserMenu;
