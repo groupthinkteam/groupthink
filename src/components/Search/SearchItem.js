1c2b60338a68d6fe34effacedf9da721ddc64b65
Dev Seth <dev.seth@duke.edu>
2020-11-05 15:19:32 +0530

    fixed search draggable bug

--- src/components/Search/SearchItem.js
+++ src/components/Search/SearchItem.js
@@ -1,9 +1,8 @@
 import React from "react"
 
 export default function SearchItem(props) {
-    console.log("matched", props.id)
     return (
-        <div id={props.id} className="search-result-item" onClick={props.onClick}>
+        <div id={"searchitem" + props.id} className="search-result-item" onClick={props.onClick}>
             <div className="matched-keywords">{props.match}</div>
             <hr className="search-result-separator"/>
         </div>
