12a27f1cbd0a81f2dbf209f44a00f28fa64e823c
Madhav Karambelkar <mady18.1998@gmail.com>
2020-11-05 13:58:25 +0530

    ReStructure PersonaList

--- src/components/PersonaList/PersonaList.js
+++ src/components/PersonaList/PersonaList.js
@@ -2,20 +2,22 @@ import React from "react"
 import { observer } from "mobx-react-lite"
 import { useStore } from "../../store/hook"
 
-import Persona from "./Persona"
-import "./PersonaList.scss"
+import "../../styles/PersonaList.scss"
 
 function PersonaList(props) {
     let store = useStore()
-
     if (!store.users) return null;
     return (
         <div className="persona-list">
-            {Object.keys(store.users)
+            {Object.entries(store.users)
                 .filter(
-                    (userID) => userID !== store.currentUser.uid && Date.now() - store.users[userID].lastUpdatedAt < 60000
+                    ([userID, _]) => userID !== store.currentUser.uid && Date.now() - store.users[userID].lastUpdatedAt < 60000
                 ).map(
-                    (userID) => <Persona key={"persona".concat(userID)} userID={userID} />
+                    ([userID, values]) =>
+                        <div key={"persona".concat(userID)} className="persona-item">
+                            <img alt={values.displayName} src={values.photoURL} />
+                            {values.displayName}
+                        </div>
                 )}
         </div>
     )
