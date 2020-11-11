76dd9563eaa9a331e2970c8119b987f52fd999f5
madhav-karambelkar <mady18.1998@gmail.com>
2020-11-12 01:11:52 +0530

    Search  Text in Text-Card added

--- src/constants/searchTemplate.js
+++ src/constants/searchTemplate.js
@@ -29,7 +29,7 @@ export default class SearchElements {
             if (key !== "root")
                 switch (val.type) {
                     case "text":
-                        projectArray.push({ id: key, text: val.content.text.replace(/<[^>]+>/g, '') });
+                        projectArray.push({ id: key, text: val.content.text.replace(/<[^>]+>/g,' ').replace(/^\s+|\s+$/g, '').split(/\s+/) });
                         break;
                     case "todo":
                         projectArray.push({ id: key, title: val.content.title })
