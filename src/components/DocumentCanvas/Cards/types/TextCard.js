406b8e0cf5a1298acb05452d8dfb69fabd539ee1
Dev Seth <dev.seth@duke.edu>
2020-11-20 16:38:57 +0530

    simplified quill modules for now

--- src/components/DocumentCanvas/Cards/types/TextCard.js
+++ src/components/DocumentCanvas/Cards/types/TextCard.js
@@ -5,29 +5,31 @@ import { useStore } from "../../../../store/hook";
 import ReactQuill from "react-quill"
 import 'react-quill/dist/quill.snow.css'
 
-/**
-* This File Saves Text And Shows it from Database .   
-* @param {*} props - Property of File .
-* @property `typeAPI` , `content` , `id`*/
-
-
 function TextCard(props) {
     const store = useStore();
     const quillRef = useRef(null);
     const me = store.cards[props.id];
-    useEffect(()=>{
+    useEffect(() => {
         if (store.currentActive === props.id && quillRef.current) {
             quillRef.current.focus();
             var quillEditor = quillRef.current.getEditor();
-            console.log("EDITOR ",quillEditor.getText().length-1, quillEditor.getSelection())
-            const selectionIndex =  quillEditor.getSelection()?.index ;
-            quillEditor.setSelection(selectionIndex ? selectionIndex : quillEditor.getText().length-1 , 0);
+            console.log("EDITOR ", quillEditor.getText().length - 1, quillEditor.getSelection())
+            const selectionIndex = quillEditor.getSelection()?.index;
+            quillEditor.setSelection(selectionIndex ? selectionIndex : quillEditor.getText().length - 1, 0);
         }
-        
-    },[props.id,store.currentActive,me.content.text]);
+    }, [props.id, store.currentActive, me.content.text]);
+
+    let modules = {
+        toolbar: ['bold', 'italic', 'underline', 'strike']
+    }
+
     return (
         <div className="text-node" style={{ overflowX: "hidden", overflowY: "auto", width: "100%", height: "100%" }}>
-            <ReactQuill   ref={quillRef} theme="snow" value={me.content.text} onChange={(value) => { props.typeAPI.saveContent(props.id, { text: value || "" }) }} />
+            <ReactQuill ref={quillRef}
+                theme="snow"
+                value={me.content.text}
+                modules={modules}
+                onChange={(value) => { props.typeAPI.saveContent(props.id, { text: value || "" }) }} />
         </div>
     )
 }
