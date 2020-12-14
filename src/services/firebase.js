ea3db1c15f728f24f6212e6f1d7f0e86e8426986
madhav-karambelkar <mady18.1998@gmail.com>
2020-12-14 16:46:23 +0530

    groupthink-test firebase_config object added

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,17 +4,26 @@ import "firebase/database"
 import 'firebase/storage';
 import 'firebase/functions'
 
+// const FIREBASE_CONFIG = {
+//     apiKey: "AIzaSyAjOQlUVvfpaPFKw_dsjVF-ZO9xAFFwLJc",
+//     authDomain: "groupthink-fc4b2.firebaseapp.com",
+//     databaseURL: "https://groupthink-fc4b2.firebaseio.com",
+//     projectId: "groupthink-fc4b2",
+//     storageBucket: "groupthink-fc4b2.appspot.com",
+//     messagingSenderId: "268412476509",
+//     appId: "1:268412476509:web:6a00e11a591b5852d4c1b4",
+//     measurementId: "G-94X9F74RYY"
+// }
 const FIREBASE_CONFIG = {
     apiKey: "AIzaSyAjOQlUVvfpaPFKw_dsjVF-ZO9xAFFwLJc",
     authDomain: "groupthink-fc4b2.firebaseapp.com",
-    databaseURL: "https://groupthink-fc4b2.firebaseio.com",
+    databaseURL: "https://groupthink-test-fc4b2-d3113.firebaseio.com/",
     projectId: "groupthink-fc4b2",
-    storageBucket: "groupthink-fc4b2.appspot.com",
+    storageBucket: "groupthink-fc4b2-test",
     messagingSenderId: "268412476509",
-    appId: "1:268412476509:web:6a00e11a591b5852d4c1b4",
-    measurementId: "G-94X9F74RYY"
-}
-
+    appId: "1:268412476509:web:921bf39858ccb0a0d4c1b4",
+    measurementId: "G-R4RZ7YFB6S"
+};
 firebase.initializeApp(FIREBASE_CONFIG);
 
 export const auth = firebase.auth;
