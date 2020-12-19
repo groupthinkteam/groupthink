e796e3d607ffa1850c6ada19753e38bb713c4f06
madhav-karambelkar <mady18.1998@gmail.com>
2020-12-19 20:00:49 +0530

    reparent at card focus

--- src/components/Arrow/HeadArrow.js
+++ src/components/Arrow/HeadArrow.js
@@ -60,6 +60,12 @@ const HeadArrow = (props) => {
                     fill={"#FF6B43"}
                 />
             </svg>
+            {/* <svg width="24"  height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12" cy="12" r="11.25" fill="#FCFBF9" stroke="#413D45" stroke-width="1.5" />
+                <svg  width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6 1V11M6 11L11 7M6 11L1 7" stroke="#413D45" stroke-width="1.5" stroke-linecap="round" />
+            </svg>
+            </svg> */}
         </>
     )
 }
