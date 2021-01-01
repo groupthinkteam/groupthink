670f61289fef3fbf7d25b6cea72b89453cb20a14
madhav-karambelkar <mady18.1998@gmail.com>
2021-01-02 01:36:08 +0530

    linepath removed from tail and midpoint

--- src/components/Arrow/MidPointInArrow.js
+++ src/components/Arrow/MidPointInArrow.js
@@ -7,7 +7,7 @@ gsap.registerPlugin(Draggable)
 
 const MidPointInArrow = (props) => {
     const [collapse, setCollapse] = useState(false);
-    const { id, midPoint, linePathDragging } = props;
+    const { id, midPoint, slopeX, slopeY } = props;
     const store = useStore();
 
     const collapseChildren = useCallback((childrenId) => {
@@ -26,28 +26,26 @@ const MidPointInArrow = (props) => {
         if (currentCard?.children) {
             Object.keys(currentCard.children).map(childId => collapseChildren(childId))
         }
-        
+
         setCollapse(!collapse)
     }, [id, store, collapse])
 
     let openEye = {
-        x:linePathDragging ? linePathDragging.x - 8 : props.slopeX - 8 ,
-        y:linePathDragging ? linePathDragging.y - 12 / 1.8 : props.slopeY - 5
+        x: slopeX - 8,
+        y: slopeY - 5
     }
     let eye = {
-        x:linePathDragging ? linePathDragging.x - 3 : props.slopeX - 3,
-        y:linePathDragging ? linePathDragging.y - 7 / 1.8 : props.slopeY-2 
+        x: slopeX - 3,
+        y: slopeY - 2
     }
-    var slope = Math.floor(props.slopeX/props.slopeY)
-    console.log("SLOPE",Math.floor(props.slopeX) ,Math.floor( props.slopeY),slope)
     return (
         <>
-            <svg onClick={()=>{ collapseChildren(id) }} style={{cursor:'pointer', position: "absolute", overflow: "visible", zIndex: -1 }} x={linePathDragging ? linePathDragging.x : midPoint.x}
-                y={linePathDragging ? linePathDragging.y : midPoint.y - 15 / 1.8} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg onClick={() => { collapseChildren(id) }} style={{ cursor: 'pointer', position: "absolute", overflow: "visible", zIndex: -1 }} x={midPoint.x}
+                y={midPoint.y - 15 / 1.8} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle
                     id={"mid".concat(id)}
-                    cx={linePathDragging ? linePathDragging.x : props.slopeX}
-                    cy={linePathDragging ? linePathDragging.y : props.slopeY}//2.15 On LEFT
+                    cx={slopeX}
+                    cy={slopeY}//2.15 On LEFT
                     // cx="12" cy="12" 
                     r="11.25" fill="#FCFBF9" stroke="#413D45" stroke-width="1.5" />
 
@@ -55,27 +53,27 @@ const MidPointInArrow = (props) => {
                     store.collapsedID[id] ?
                         <>
                             <svg
-                                x={linePathDragging ? linePathDragging.x - 8 : props.slopeX - 8} y={linePathDragging ? linePathDragging.y - 3 / 1.8 : props.slopeY - 6 / 1.8}
+                                x={slopeX - 8} y={slopeY - 6 / 1.8}
                                 width="16" height="6" viewBox="0 0 16 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1.58398 1C1.58398 1 3.91732 4.5 8.00065 4.5C12.084 4.5 14.4173 1 14.4173 1" stroke="#413D45" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                             <svg
-                                x={linePathDragging ? linePathDragging.x - 10 : props.slopeX - 10} y={linePathDragging ? linePathDragging.y - 1 / 1.8 : props.slopeY - 4 / 1.8}
+                                x={slopeX - 10} y={slopeY - 4 / 1.8}
                                 width="5" height="5" viewBox="0 0 5 5" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M3.33398 1.33203L1.58398 3.66536" stroke="#413D45" stroke-width="1.5" stroke-linecap="round" />
                             </svg>
                             <svg
-                                x={linePathDragging ? linePathDragging.x - 5 : props.slopeX - 5} y={linePathDragging ? linePathDragging.y + 3 / 1.8 : props.slopeY }
+                                x={slopeX - 5} y={slopeY}
                                 width="5" height="5" viewBox="0 0 5 5" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M3.33398 1.33203L1.58398 3.66536" stroke="#413D45" stroke-width="1.5" stroke-linecap="round" />
                             </svg>
                             <svg
-                                x={linePathDragging ? linePathDragging.x : props.slopeX} y={linePathDragging ? linePathDragging.y + 3 / 1.8 : props.slopeY }
+                                x={slopeX} y={slopeY}
                                 width="4" height="5" viewBox="0 0 4 5" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1.45898 1.50008L2.60471 4.18229" stroke="#413D45" stroke-width="1.5" stroke-linecap="round" />
                             </svg>
                             <svg
-                                x={linePathDragging ? linePathDragging.x + 5 : props.slopeX + 5} y={linePathDragging ? linePathDragging.y - 2 / 1.8 : props.slopeY - 5 / 1.8}
+                                x={slopeX + 5} y={slopeY - 5 / 1.8}
                                 width="4" height="5" viewBox="0 0 4 5" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1.45898 1.50008L2.60471 4.18229" stroke="#413D45" stroke-width="1.5" stroke-linecap="round" />
                             </svg>
@@ -92,9 +90,6 @@ const MidPointInArrow = (props) => {
                         </>
 
                 }
-
-
-
             </svg>
 
         </>
--- src/components/Arrow/TailArrow.js
+++ src/components/Arrow/TailArrow.js
@@ -6,47 +6,38 @@ import { observer } from 'mobx-react-lite';
 gsap.registerPlugin(Draggable)
 
 const TailArrow = (props) => {
-    const { id, tail, linePathDragging } = props;
+    const { id, tail } = props;
     const store = useStore();
     const bottomSVG = (
         <svg
-            x={linePathDragging ? linePathDragging.x - 8 : tail.x - 8}
-            id={"tail".concat(props.id)} y={linePathDragging ? linePathDragging.y - 20 : tail.y - 20} width="16" height="22" viewBox="0 0 16 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            {
-                !linePathDragging ?
-                    <path opacity="0.8" d="M0 1C9 0.447715 8.55228 -2.41411e-08 8 0C7.44772 2.41411e-08 7 0.447715 7 1L9 1ZM7.29289 21.7071C7.68342 22.0976 8.31658 22.0976 8.70711 21.7071L15.0711 15.3431C15.4616 14.9526 15.4616 14.3195 15.0711 13.9289C14.6805 13.5384 14.0474 13.5384 13.6569 13.9289L8 19.5858L2.34315 13.9289C1.95262 13.5384 1.31946 13.5384 0.928933 13.9289C0.538408 14.3195 0.538408 14.9526 0.928933 15.3431L7.29289 21.7071ZM7 1L7 21L9 21L9 1L7 1Z" fill="#5FA2F1" />
-                    : null
-            }
+            x={tail.x - 8}
+            id={"tail".concat(props.id)} y={tail.y - 20} width="16" height="22" viewBox="0 0 16 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path opacity="0.8" d="M0 1C9 0.447715 8.55228 -2.41411e-08 8 0C7.44772 2.41411e-08 7 0.447715 7 1L9 1ZM7.29289 21.7071C7.68342 22.0976 8.31658 22.0976 8.70711 21.7071L15.0711 15.3431C15.4616 14.9526 15.4616 14.3195 15.0711 13.9289C14.6805 13.5384 14.0474 13.5384 13.6569 13.9289L8 19.5858L2.34315 13.9289C1.95262 13.5384 1.31946 13.5384 0.928933 13.9289C0.538408 14.3195 0.538408 14.9526 0.928933 15.3431L7.29289 21.7071ZM7 1L7 21L9 21L9 1L7 1Z" fill="#5FA2F1" />
         </svg>
     )
     const leftSVG = (
         <svg id={"tail".concat(props.id)}
-            x={linePathDragging ? linePathDragging.x - 27 : tail.x - 27}
-            y={linePathDragging ? linePathDragging.y : tail.y - 25} width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            {
-                !linePathDragging ?
-                    <path opacity="0.8" d="M19 7C19.5523 7 20 7.44772 20 8C20 8.55228 19.5523 9 19 9V7ZM0.292892 8.70711C-0.0976315 8.31658 -0.0976315 7.68342 0.292892 7.29289L6.65685 0.928932C7.04738 0.538408 7.68054 0.538408 8.07107 0.928932C8.46159 1.31946 8.46159 1.95262 8.07107 2.34315L2.41421 8L8.07107 13.6569C8.46159 14.0474 8.46159 14.6805 8.07107 15.0711C7.68054 15.4616 7.04738 15.4616 6.65685 15.0711L0.292892 8.70711ZM19 9H1V7H19V9Z" fill="#5FA2F1" />
-                    : null
-            }
+            x={tail.x - 27}
+            y={tail.y - 25} width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+            <path opacity="0.8" d="M19 7C19.5523 7 20 7.44772 20 8C20 8.55228 19.5523 9 19 9V7ZM0.292892 8.70711C-0.0976315 8.31658 -0.0976315 7.68342 0.292892 7.29289L6.65685 0.928932C7.04738 0.538408 7.68054 0.538408 8.07107 0.928932C8.46159 1.31946 8.46159 1.95262 8.07107 2.34315L2.41421 8L8.07107 13.6569C8.46159 14.0474 8.46159 14.6805 8.07107 15.0711C7.68054 15.4616 7.04738 15.4616 6.65685 15.0711L0.292892 8.70711ZM19 9H1V7H19V9Z" fill="#5FA2F1" />
+
         </svg>
     )
     const rightSVG = (
-        <svg id={"tail".concat(props.id)} x={linePathDragging ? linePathDragging.x - 8 : tail.x}
-            y={linePathDragging ? linePathDragging.y : tail.y - 27} width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            {
-                !linePathDragging ?
-                    <path opacity="0.8" d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM19.7071 8.70711C20.0976 8.31658 20.0976 7.68342 19.7071 7.29289L13.3431 0.928932C12.9526 0.538408 12.3195 0.538408 11.9289 0.928932C11.5384 1.31946 11.5384 1.95262 11.9289 2.34315L17.5858 8L11.9289 13.6569C11.5384 14.0474 11.5384 14.6805 11.9289 15.0711C12.3195 15.4616 12.9526 15.4616 13.3431 15.0711L19.7071 8.70711ZM1 9H19V7H1V9Z" fill="#32AAFF" />
-                    : null
-            }
+        <svg id={"tail".concat(props.id)} x={tail.x}
+            y={tail.y - 27} width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+            <path opacity="0.8" d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM19.7071 8.70711C20.0976 8.31658 20.0976 7.68342 19.7071 7.29289L13.3431 0.928932C12.9526 0.538408 12.3195 0.538408 11.9289 0.928932C11.5384 1.31946 11.5384 1.95262 11.9289 2.34315L17.5858 8L11.9289 13.6569C11.5384 14.0474 11.5384 14.6805 11.9289 15.0711C12.3195 15.4616 12.9526 15.4616 13.3431 15.0711L19.7071 8.70711ZM1 9H19V7H1V9Z" fill="#32AAFF" />
+
         </svg>
     )
     const topSVG = (
-        <svg id={"tail".concat(props.id)} x={linePathDragging ? linePathDragging.x - 8 : tail.x}
-            y={linePathDragging ? linePathDragging.y : tail.y - 27} width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            {
-                !linePathDragging ?
-                    <path opacity="0.8" d="M9 19C9 19.5523 8.55228 20 8 20C7.44772 20 7 19.5523 7 19H9ZM7.29289 0.292892C7.68342 -0.0976315 8.31658 -0.0976315 8.70711 0.292892L15.0711 6.65685C15.4616 7.04738 15.4616 7.68054 15.0711 8.07107C14.6805 8.46159 14.0474 8.46159 13.6569 8.07107L8 2.41421L2.34315 8.07107C1.95262 8.46159 1.31946 8.46159 0.928932 8.07107C0.538408 7.68054 0.538408 7.04738 0.928932 6.65685L7.29289 0.292892ZM7 19V1H9V19H7Z" fill="#5FA2F1" />
-                    : null}
+        <svg id={"tail".concat(props.id)} x={tail.x}
+            y={tail.y - 27} width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+            <path opacity="0.8" d="M9 19C9 19.5523 8.55228 20 8 20C7.44772 20 7 19.5523 7 19H9ZM7.29289 0.292892C7.68342 -0.0976315 8.31658 -0.0976315 8.70711 0.292892L15.0711 6.65685C15.4616 7.04738 15.4616 7.68054 15.0711 8.07107C14.6805 8.46159 14.0474 8.46159 13.6569 8.07107L8 2.41421L2.34315 8.07107C1.95262 8.46159 1.31946 8.46159 0.928932 8.07107C0.538408 7.68054 0.538408 7.04738 0.928932 6.65685L7.29289 0.292892ZM7 19V1H9V19H7Z" fill="#5FA2F1" />
+
         </svg>
     )
     const displayTail = () => {
@@ -62,12 +53,12 @@ const TailArrow = (props) => {
         }
     }
     var topCircle = {
-        x: linePathDragging ? linePathDragging.x - 8 : tail.x - 13,
-        y: linePathDragging ? linePathDragging.y - 20 : tail.y - 40
+        x: tail.x - 13,
+        y: tail.y - 40
     }
     var oneCross = {
-        x: linePathDragging ? linePathDragging.x - 8 : tail.x - 7,
-        y: linePathDragging ? linePathDragging.y - 20 : tail.y - 33
+        x: tail.x - 7,
+        y: tail.y - 33
     }
     switch (tail.position) {
         case "right":
@@ -91,7 +82,7 @@ const TailArrow = (props) => {
 
 
             {
-                props.showArrowButtons && !linePathDragging ?
+                props.showArrowButtons ?
                     <>
                         <svg
                             cursor="pointer" onClick={() => store.reparentCard(id, 'root')}
