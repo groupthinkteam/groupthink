302a2a116787da0c53b260d1706314f631453429
Dev Seth <dev.seth@duke.edu>
2021-01-05 19:30:35 +0530

    user menu styling

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -6,24 +6,22 @@ import '../../styles/UserMenu.scss'
 
 const UserMenu = (props) => {
   let store = useStore();
-    return (
-        <div className="user-menu-container">
-              <div className="colorblack">
-                <h4>{store.currentUser.displayName}</h4>
-              </div>
-              <div className="info">
-                <img className="ea-logo" src={require("../../assets/menu/ealogo.svg")} alt="early access"/>
-              </div>
-              <hr className="separator" />
-              <div className="options">
-              <Link to="/dashboard" className="colorblack">Dashboard</Link>
-                <br />
-                <Link to="/settings" className="colorblack">Settings</Link>
-                <br />
-                <a href="true" onClick={props.signOut} className="colororange">Logout</a>
-              </div>
-        </div>
-    );
+  return (
+    <div className="user-menu-container">
+      <div className="colorblack">
+        <h4>{store.currentUser.displayName}</h4>
+      </div>
+      <div className="info">
+        <img className="ea-logo" src={require("../../assets/menu/ealogo.svg")} alt="early access" />
+      </div>
+      <hr className="separator" />
+      <div className="options">
+        <Link to="/dashboard" className="option">Dashboard</Link>
+        <Link to="/settings" className="option">Settings</Link>
+        <a href="true" onClick={props.signOut} className="option danger">Logout</a>
+      </div>
+    </div>
+  );
 };
 
 export default observer(UserMenu);
